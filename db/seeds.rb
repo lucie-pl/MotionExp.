117e1673785157e5e4c355c9eeb8069dbb462094
Hongri Ma <mahongri1993@gmail.com>
2023-12-05 17:24:59 -0500

    notification

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+SaveItem.destroy_all
+User.destroy_all
+Notification.destroy_all
+
+User.create(first_name: "Lorry", last_name: "Brossard", username: "Lorry", email: "lorry_b@gmail.com", password:"123456")
+
+SaveItem.create(user: User.first, title: "Inception", year: 2010, api_movie_id: 1, poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg")
+SaveItem.create(user: User.first, title: "Interstellar", year: 2014, api_movie_id: 2, poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg")
+
+
+Notification.create(notif_title: "new news", content: "the hunger game is coming", save_item: SaveItem.first, user: User.first)
