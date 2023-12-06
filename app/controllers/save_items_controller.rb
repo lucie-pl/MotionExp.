ec1a684db88ea3972661b8c8b3d9e482c53e8ede
Lucie Plantier <lucie.plantier@live.fr>
2023-12-06 17:49:07 -0500

    Search bar done

--- app/controllers/save_items_controller.rb
+++ app/controllers/save_items_controller.rb
@@ -1,2 +1,17 @@
 class SaveItemsController < ApplicationController
+
+  def index
+    @save_items = SaveItem.all
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        movies.title ILIKE :query
+        OR movies.category ILIKE :query
+        OR directors.first_name ILIKE :query
+        OR directors.last_name ILIKE :query
+        OR actors.first_name ILIKE :query
+        OR actors.last_name ILIKE :query
+      SQL
+      @save_items = @save_items.joins(:directors).joins(:actors).where(sql_subquery, query: "%#{params[:query]}%").distinct
+    end
+  end
 end
