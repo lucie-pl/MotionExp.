3be2c83940833f91fd716c573bacbf9c937cd4e6
Hongri Ma <mahongri1993@gmail.com>
2023-12-06 14:39:23 -0500

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,20 @@
 class ReviewsController < ApplicationController
   def new
+    @save_item = Save_item.find(:save_item_id)
     @review = Review.new
   end
 
   def create
+    raise
+    @save_item = Save_item.find(:save_item_id)
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
   end
 end
