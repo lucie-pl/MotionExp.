9b4b0350f3120d2d63cd434a224a75a35a296bb7
Lorry Brossard <lorrybrossard@hotmail.fr>
2023-12-06 17:10:41 -0500

    add to Wl and history list - create save item

--- app/controllers/save_items_controller.rb
+++ app/controllers/save_items_controller.rb
@@ -1,2 +1,21 @@
 class SaveItemsController < ApplicationController
+
+  def create
+    @save_item = SaveItem.new(save_item_params)
+    @save_item.api_movie_id = params[:movie_api_id]
+    @save_item.user = current_user
+    if @save_item.save
+      redirect_to movie_path(params[:movie_api_id])
+    else
+      @movie = Movie.find(params[:movie_api_id])
+      render "movies/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def save_item_params
+    params.require(:save_item).permit(:title, :poster, :year, :history)
+  end
+
 end
