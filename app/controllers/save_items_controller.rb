fdafe26fa1618f2eed65a698874e59591749dc9a
Hongri Ma <mahongri1993@gmail.com>
2023-12-07 15:58:47 -0500

    search in watchlist and history

--- app/controllers/save_items_controller.rb
+++ app/controllers/save_items_controller.rb
@@ -1,23 +1,15 @@
 class SaveItemsController < ApplicationController
 
   def index
-    save_items = SaveItem.all
+    save_items = SaveItem.where(user: current_user)
     @history_items = save_items.select { |save_item| save_item.history }
     @watchlist_items = save_items.reject { |save_item| save_item.history }
 
-    # Might not work with the separation between watchlist and history list ! TODO !!!!!!!
-    # if params[:query].present?
-    #   sql_subquery = <<~SQL
-    #     movies.title ILIKE :query
-    #     OR movies.category ILIKE :query
-    #     OR directors.first_name ILIKE :query
-    #     OR directors.last_name ILIKE :query
-    #     OR actors.first_name ILIKE :query
-    #     OR actors.last_name ILIKE :query
-    #   SQL
-    #   save_items = save_items.joins(:directors).joins(:actors).where(sql_subquery, query: "%#{params[:query]}%").distinct
-    # end
-
+    if params[:history_query].present?
+      @history_items = Movie.find(@history_items).select { |item| item.title.downcase.include?(params[:history_query].downcase) }
+    elsif params[:watchlist_query].present?
+      @watchlist_items = Movie.find(@watchlist_items).select { |item| item.title.downcase.include?(params[:watchlist_query].downcase) }
+    end
   end
 
   def create
