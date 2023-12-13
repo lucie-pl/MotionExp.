f06a54c272bec45e4b3da15506293a603f88b98f
Hongri Ma <mahongri1993@gmail.com>
2023-12-13 15:23:55 -0500

    chat in everypag

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,8 @@
 class FriendshipsController < ApplicationController
+  def index
+    @friendships = Friendship.for_user(current_user.id)
+  end
+
   def show
     @friendship = Friendship.find(params[:id])
     @message = Message.new
--- db/migrate/20231212201633_create_messages.rb
+++ db/migrate/20231212201633_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[7.1]
   def change
     create_table :messages do |t|
-      t.text :content
+      t.string :content
       t.references :friendship, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
