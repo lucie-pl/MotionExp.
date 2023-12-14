3ef248e78c97fe80df3dc7972a8d0f24c2d6d2ca
Hongri Ma <mahongri1993@gmail.com>
2023-12-14 11:51:41 -0500

    css about chating

--- app/javascript/controllers/friendship_subscription_controller.js
+++ app/javascript/controllers/friendship_subscription_controller.js
@@ -3,7 +3,7 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="friendship-subscription"
 export default class extends Controller {
-  static values = { friendshipId: Number }
+  static values = { friendshipId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -24,7 +24,28 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+     // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
